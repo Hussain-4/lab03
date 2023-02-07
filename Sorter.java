69b402896c2621179e1c0d07128794f32a60e50d
Abdullrahman <Abdullrahman.chaabouni123@gmail.com>
2023-02-07 16:52:19 +0300

    v2

--- Sorter.java
+++ Sorter.java
@@ -1,19 +1,32 @@
 public class Sorter {
-    public static void main(String[] args) {
+    // Java program for implementation of Insertion Sort
+    /* Function to sort array using insertion sort */
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
 
-    }
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;//searching for lowest index
-                }
+            /*
+             * Move elements of arr[0..i-1], that are
+             * greater than key, to one position ahead
+             * of their current position
+             */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            arr[j + 1] = key;
         }
     }
-}
+
+    /* A utility function to print array of size n */
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+};
