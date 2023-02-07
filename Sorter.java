16fabc6e384b6b0ff59a9e0de8e6df182ffc1fcb
Abdullrahman <Abdullrahman.chaabouni123@gmail.com>
2023-02-07 17:09:30 +0300

    v2

--- Sorter.java
+++ Sorter.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 
 public class Sorter {
     public static void main(String[] args) {
-        int[] arr = {1,5,3,8,6,2};
+        int[] arr = { 1, 5, 3, 8, 6, 2 };
         selectionSort(arr);
     }
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;//searching for lowest index
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;// searching for lowest index
                 }
             }
             int smallerNumber = arr[index];
@@ -19,6 +19,25 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /*
+             * Move elements of arr[0..i-1], that are
+             * greater than key, to one position ahead
+             * of their current position
+             */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }
 
 };
